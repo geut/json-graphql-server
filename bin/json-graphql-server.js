6d60f943f957706b431a4ed168acd08bb827fcf4
Martín Acosta <tinchoz49@gmail.com>
2018-06-26 19:56:31 -0300

    fixed example

--- bin/json-graphql-server.js
+++ bin/json-graphql-server.js
@@ -4,7 +4,6 @@ var path = require('path');
 var express = require('express');
 var cors = require('cors');
 var JsonGraphqlServer = require('../index.js').default;
-
 var dataFilePath = process.argv.length > 2 ? process.argv[2] : './data.json';
 var data = require(path.join(process.cwd(), dataFilePath));
 var PORT = process.env.NODE_PORT || 3000;
